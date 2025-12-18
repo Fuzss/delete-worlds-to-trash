e8905b0afb1ac7d5f158c16c986d8893c124ec18
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-12-18 12:02:57 +0100

    prepare 1.21.11 port

---
+++ 1.21.11/Fabric/src/main/java/fuzs/deleteworldstotrash/fabric/DeleteWorldsToTrashFabric.java
@@ -0,0 +1,11 @@
+package fuzs.deleteworldstotrash.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DeleteWorldsToTrashFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        // NO-OP
+    }
+}
